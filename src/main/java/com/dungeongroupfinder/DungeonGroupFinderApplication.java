cfbb47ec3146a40a77bc7a23aa363b113e11d5f0
kkolodziej <kkolodziej@microstrategy.com>
2020-06-26 14:00:02 +0200

    remove comments

--- src/main/java/com/dungeongroupfinder/DungeonGroupFinderApplication.java
+++ src/main/java/com/dungeongroupfinder/DungeonGroupFinderApplication.java
@@ -18,25 +18,4 @@ public class DungeonGroupFinderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DungeonGroupFinderApplication.class, args);
 	}
-	// move to Repository
-	@Autowired
-	private PlayerRepository playerRepository;
-
-	/*@Override
-	public void run (String ...args) {
-		List<Player> playerList1 = playerRepository.findAll();
-		playerList1.forEach(System.out::println);
-
-		Player Adam = getPlayer();
-
-		playerRepository.save(Adam);
-
-		List<Player> playerList2 = playerRepository.findAll();
-		playerList2.forEach(System.out::println);
-
-	}
-
-	private Player getPlayer() {
-		return new Player("Adam", 3);
-	}*/
 }
