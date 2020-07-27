ffd31e1b7e164fcaee1a172ef8d3fedc119b803e
kkolodziej <kkolodziej@microstrategy.com>
2020-07-27 22:37:21 +0200

    integration tests config increment

--- src/test/java/com/dungeongroupfinder/MySQLContainerCreation.java
+++ src/test/java/com/dungeongroupfinder/MySQLContainerCreation.java
@@ -3,8 +3,12 @@ package com.dungeongroupfinder;
 import com.dungeongroupfinder.entities.Player;
 import com.dungeongroupfinder.enums.Roles;
 import com.dungeongroupfinder.repository.PlayerRepository;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,8 +16,12 @@ import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.MySQLContainer;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -23,9 +31,9 @@ public class MySQLContainerCreation {
 
     @ClassRule
     public static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.0.21")
-            .withDatabaseName("integration-tests-db")
-            .withUsername("name")
-            .withPassword("pass");
+            .withUsername("root")
+            .withPassword("");
+            //default DB name - test
 
     static class Initializer
             implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -43,11 +51,22 @@ public class MySQLContainerCreation {
         System.out.println("hello");
     }
 
+   // @BeforeAll
+   // @Sql(scripts = {"", "/Users/kkolodziej/dumps/Dump20200727/dungeonGroupFinder_players.sql"})
+    //public static void dupa() {
+       // mySQLContainer.withInitScript("/Users/kkolodziej/dumps/Dump20200727/dungeonGroupFinder_guilds.sql");
+    //}
+
     @Test
     public void konsti() {
-        insertUsers(); // PRECONFIGURE DATABASE TO CONTAIN COLUMNS
-        int a = playerRepository.findAll().size();
-        assertEquals(a, 1);
+       // insertUsers(); // PRECONFIGURE DATABASE TO CONTAIN COLUMNS
+       // List<Player> d = playerRepository.findAll();
+        //int a = d.size();
+        //assertEquals(a, 6);
+        mySQLContainer.withInitScript("/Users/kkolodziej/dumps/Dump20200727/dungeonGroupFinder_players.sql");
+        System.out.println(playerRepository.showTables());
+        System.out.println(playerRepository.findAll());
+
     }
 
     @Autowired
@@ -58,3 +77,9 @@ public class MySQLContainerCreation {
     }
 
 }
+
+/*
+ dziedziczyc inne testy po tej klasie zeby baza odpalala sie raz
+ przeniesc .sql tutaj i zkomitowac
+ dodac use database do dumpa / sprobowac @sql
+* */
